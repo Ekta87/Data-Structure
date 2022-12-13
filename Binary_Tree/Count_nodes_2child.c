6875607c297fcdb2db16d295b90863d98faff7d3
Ekta87 <114105092+Ekta87@users.noreply.github.com>
2022-12-13 18:51:35 +0530

    Add files via upload

---
+++ Binary_Tree/Count_nodes_2child.c
@@ -0,0 +1,54 @@
+// Program to find count of nodes having 2 children
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left, *right;
+};
+struct node *MakeNode(char x)
+{
+    struct node *p;
+    p = (struct node *)malloc(sizeof(struct node));
+    p->data = x;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+}
+
+int Count_n2(struct node *tree)
+{
+    if (tree == NULL)
+    {
+        return 0;
+    }
+    else if ((tree->left == NULL) && (tree->right == NULL))
+    {
+        return 0;
+    }
+    else if ((tree->left != NULL) && (tree->right != NULL))
+    {
+        return 1+Count_n2(tree->left) + Count_n2(tree->right);
+    }
+    else
+    {
+        return Count_n2(tree->left) + Count_n2(tree->right);
+    }
+}
+
+void main()
+{
+    struct node *root = NULL;
+    int x;
+    root = MakeNode(1);
+    root->left = MakeNode(2);
+    root->right = MakeNode(3);
+    root->left->left = MakeNode(5);
+    root->right->left = MakeNode(4);
+    root->right->right = MakeNode(6);
+    printf("Total Number of nodes having 2 children: ");
+    x = Count_n2(root);
+    printf("%d", x);
+}
